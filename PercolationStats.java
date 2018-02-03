7e8e3ee12cfb0806d10a1426a752fb847519aca2
Gordon <gordlonious@gmail.com>
2018-02-03 14:38:26 -0700

    full proposal for PercolationStats

--- PercolationStats.java
+++ PercolationStats.java
@@ -1,5 +1,6 @@
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
+import java.lang.Math;
 public class PercolationStats {
 	private int times, n;
 	private Percolation[] p;
@@ -18,32 +19,51 @@ public class PercolationStats {
 	public double mean() {
 		double[] psThresh = new double[openCount.length];
 		for (int i = 0; i < psThresh.length; i++) {
-			psThresh[i] = openCount[i] / (n*n);
+			double tmpCount = new Double(openCount[i]);
+			double tmpSize = new Double((n*n));
+			psThresh[i] = tmpCount/tmpSize;
 		}
 		return StdStats.mean(psThresh);
-	} // return sample mean (avg over all T)
+	}
 	public double stddev() { 
 		double[] psThresh = new double[openCount.length];
 		for (int i = 0; i < psThresh.length; i++) {
-			psThresh[i] = ((openCount[i] / (n*n)) * (openCount[i] / (n*n)));
+			double oc = new Double(openCount[i]);
+			double s = new Double(n*n);
+			psThresh[i] = new Double((oc / s) * (oc / s));
 		}
 		return StdStats.stddev(psThresh);
-	} // return standard deviation (avg over all T)
-	public double confidenceLow() { return 0.00; }
-	public double confidenceHigh() { return 0.00; }
+	}
+	public double confidenceLow() { 
+		double m = this.mean();
+		double tstddev = new Double(1.96 * this.stddev());
+		double sqrtN = Math.sqrt(this.n);
+		return new Double(m - (tstddev / sqrtN));
+	}
+	public double confidenceHigh() { 
+		double m = this.mean();
+		double tstddev = new Double(1.96 * this.stddev());
+		double sqrtN = Math.sqrt(this.n);
+		return new Double(m + (tstddev / sqrtN));
+	}
 	private double threshold(int index) {
 		return (this.openCount[index] / this.n);
 	}
 	private void runExperiment(int index) {
 		int tOpenCount = 0;
 		while(!this.p[index].percolates()) {
-			this.p[index].open(StdRandom.uniform(0, this.n), StdRandom.uniform(0, this.n));
-			tOpenCount++;
+			int i = StdRandom.uniform(0, this.n);
+			int j = StdRandom.uniform(0, this.n);
+			if(!this.p[index].isOpen(i, j)) {
+				this.p[index].open(i, j);
+				tOpenCount++;
+			}
 		}
 		this.openCount[index] = tOpenCount;
 	}
 	public static void main(String[] args) {
 		PercolationStats ps = new PercolationStats(200, 100);
-		System.out.println(ps.mean());
+		System.out.printf(" mean was: %.15f%n standard deviation was: %.15f%n confidenceLow was: %.15f%n confidenceHigh was: %.15f%n", ps.mean(), ps.stddev(), ps.confidenceLow(), ps.confidenceHigh());
 	}
+	
 }
