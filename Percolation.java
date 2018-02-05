76039a5fa074dd89013c0603622c140a30b5e419
Gordon <gordlonious@gmail.com>
2018-02-04 17:16:41 -0700

    Approximation of what I submitted to canvas -- should be identical

--- Percolation.java
+++ Percolation.java
@@ -1,16 +1,20 @@
-//package Percolation.Percolation;
-
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-import java.lang.IndexOutOfBoundsException;
-import java.lang.IllegalArgumentException;
-import java.util.*;
 
-public class Percolation {
-  WeightedQuickUnionUF wUnionUF;
+/**
+ * @author Aidan Hubert,
+ * Gordon Portzline
+ */
+public class Percolation 
+{
+  private WeightedQuickUnionUF wUnionUF, fullQuickUnionUF;
   private boolean[][] openSites;
   private int gridSize, top, bottom;
 
-  public Percolation(int n) {
+  /**
+   * Creates a new percolation simulation, with all sites initialized to blocked
+   */
+  public Percolation(int n) 
+  {
     if (n <= 0)
       throw new IllegalArgumentException("Illegal Percolation parameter");
     this.gridSize = n;
@@ -19,41 +23,69 @@ public class Percolation {
     // initialize all sites to a blocked status
     this.openSites = new boolean[n][n];
     wUnionUF = new WeightedQuickUnionUF((n * n) + 5);
+    fullQuickUnionUF = new WeightedQuickUnionUF((n * n) + 5);
     for (int i = 0; i < n; i++)
+    {
       wUnionUF.union(twoDToOneD(i,0), top);
+      fullQuickUnionUF.union(twoDToOneD(i,0), top);
+    }
     for (int i = 0; i < n; i++)
       wUnionUF.union(twoDToOneD(i,n-1), bottom);
   }
-
-  public void open(int row, int col) {
+  /**
+   * Opens a specific site, and unions it to any surrounding open sites.
+   */
+  public void open(int row, int col) 
+  {
+    if(openSites[col][row])
+      return;
     if (row < 0 || row > (gridSize - 1) || col < 0 || col > (gridSize - 1))
       throw new IndexOutOfBoundsException("Open parameter(s) out of range");
     openSites[col][row] = true;
     if (isInBounds(row, col-1) && isOpen(row, col - 1))
+    {
       wUnionUF.union(twoDToOneD(col, row), twoDToOneD(col - 1, row));
+      fullQuickUnionUF.union(twoDToOneD(col, row), twoDToOneD(col - 1, row));
+    }
     if (isInBounds(row, col+1) && isOpen(row, col + 1))
+    {
       wUnionUF.union(twoDToOneD(col, row), twoDToOneD(col + 1, row));
+      fullQuickUnionUF.union(twoDToOneD(col, row), twoDToOneD(col + 1, row));
+    }
     if (isInBounds(row-1, col) && isOpen(row - 1, col))
+    {
       wUnionUF.union(twoDToOneD(col, row),twoDToOneD(col, row - 1));
+      fullQuickUnionUF.union(twoDToOneD(col, row),twoDToOneD(col, row - 1));
+    }
     if (isInBounds(row+1, col) && isOpen(row + 1, col))
+    {
       wUnionUF.union(twoDToOneD(col, row),twoDToOneD(col, row + 1));
+      fullQuickUnionUF.union(twoDToOneD(col, row),twoDToOneD(col, row + 1));
+    }
   }
- 
-  public boolean isOpen(int row, int col) {
+   /**
+   * Checkes if specific site is open, and returns the result
+   */
+  public boolean isOpen(int row, int col) 
+  {
     // has been opened, can now count as connecting other nodes together
     if (row < 0 || row > (gridSize - 1) || col < 0 || col > (gridSize - 1))
       throw new IndexOutOfBoundsException("IsOpen parameter(s) out of range");
     return openSites[col][row];
   }
-
-  public boolean isFull(int row, int col) {
-    // connected to open site in top row
+  /**
+   * Checks if a specific site is connected to open site in top row
+   */
+  public boolean isFull(int row, int col) 
+  {
     int i = twoDToOneD(col, row);
-    return isOpen(row, col) && wUnionUF.connected(i, top);
+    return isOpen(row, col) && fullQuickUnionUF.connected(i, top);
   }
-
-  public boolean percolates() {
-    // is there a Full node on the bottom row from which a path exists to the top row
+  /**
+   * Checks if the system percolates
+   */
+  public boolean percolates() 
+  {
     return wUnionUF.connected(bottom, top);
   }
 
@@ -62,23 +94,8 @@ public class Percolation {
     return !(row < 0 || row > (gridSize - 1) || col < 0 || col > (gridSize - 1));
   }
 
-  private int twoDToOneD(int x, int y) {
-    return y + (x * gridSize);
-  }
-
-  public static void main(String args[]) 
+  private int twoDToOneD(int x, int y) 
   {
-    // test percolation class
-    Scanner input = new Scanner(System.in);
-    Percolation p = new Percolation(10);
-    for (int i = 0; i < 10; i++) 
-    {
-      p.open(i, 5);
-    }
-    //System.out.println(p.wUnionUF.find(104));
-    //System.out.println(p.IsFull(5, 5));
-    System.out.println(p.percolates());
-
-    input.close();
+    return y + (x * gridSize);
   }
 }
